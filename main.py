c3feea5931cf43f3f464613a0d9b7158466077de
Gustavo :D <70520011+narasai10@users.noreply.github.com>
2022-03-26 18:05:54 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+
+print('Bem vindo plebeu, ao wilson códigos, por enquanto eu só sei hipotenusa')
+cateto1 = float(input('Comprimento do primeiro cateto (| <- esse)'))
+cateto2 = float(input('Comprimento do segundo cateto (_ <- esse aqui)'))
+hip = (cateto1 ** 2 + cateto2 ** 2) ** (1/2)
+print('Se o idiota que me fez, não programou nada de errado, a hipotenusa é {0}'.format(hip))
